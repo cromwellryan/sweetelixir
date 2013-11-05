a528efa41bd5f45eaf6105101ca291dfdff885ab
Ryan Cromwell <ryan@echelontouch.com>
2013-11-04 23:54:21 -0500

    cleaned up app output

--- src/counter/lib/counter/worker.ex
+++ src/counter/lib/counter/worker.ex
@@ -6,19 +6,29 @@ defmodule Counter.Worker do
     { :ok, initial_value }
   end
 
+  def start_link() do
+    IO.puts ":start_link"
+    :gen_server.start_link( { :global, :counter }, __MODULE__, 0, [] )
+  end
+
   def handle_call(:stat, _from, count ) do
-    IO.puts ":stat - #{count}"
+    IO.puts "stat - #{count}"
 
     { :reply, count, count }
   end
 
   def handle_cast(:increment, count) do
-    IO.puts ":increment - #{count + 1}"
+    IO.puts "increment - #{count + 1}"
     { :noreply, count + 1 }
   end
 
-  def start_link() do
-    IO.puts ":start_link"
-    :gen_server.start_link( { :global, :counter }, __MODULE__, 0, [] )
+  def handle_info(:timeout, _state) do
+    IO.puts "handle_timeout"
+  end
+
+  def handle_info(_msg, _state) do
+  end
+
+  def terminate(_reason, _state) do
   end
 end
