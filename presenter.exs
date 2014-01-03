484d43646240b665ea30d070ed023572a59526fd
Ryan Cromwell <ryan@heysparkbox.com>
2014-01-03 10:22:06 -0500

    presenter links for most basics

--- presenter.exs
+++ presenter.exs
@@ -1,9 +1,50 @@
 defmodule Presenter do
-  def start do
-    open '07-basics/README.md#basics'
+  @moduledoc """
+  Presenter can be used in `iex` to open sections of the session.
+
+  ## Example Usage
+
+      $ iex presenter.exs
+
+      iex> import Presenter; import Presenter.Basics
+      nil
+
+      iex> start
+      nil
+
+      iex> tools
+      nil
+
+      iex> Basics.pipeline
+      nil
+  """
+  def start, do: open "README.md#getting-started"
+  def setup, do: open "setup/README.md"
+  def tools, do: open "tools/README.md"
+
+  def basics, do: open "basics/README.md"
+
+  defmodule Basics do
+
+    def operators, do: open "operators"
+    def types, do: open "types"
+    def numbers, do: open "operators"
+    def boolean, do: open "boolean"
+    def atoms, do: open "atoms"
+    def list, do: open "lists"
+    def tuples, do: open "tuples"
+    def strings, do: open "strings"
+    def keywordlists, do: open "keyword-lists"
+    def equality, do: open "equality"
+    def functions, do: open "functions"
+    def modules, do: open "modules"
+
+    defp open(hash), do: Presenter.open "basics/README.md##{hash}"
+
   end
 
-  defp open(section) do
+  def open(section) do
     System.cmd 'open https://github.com/cromwellryan/sweetelixir/blob/master/#{section}'
+    nil
   end
 end
